3ed95d2a8aeda356a90169d1569a548398b018af
Rahul Pareek <41017269+bisomaticc@users.noreply.github.com>
2026-02-28 02:46:32 +0530

    Update product.service.js

--- services/product.service.js
+++ services/product.service.js
@@ -1,8 +1,9 @@
 const Product = require("../models/product.models");
 
 exports.getAll = () => Product.find();
-exports.getById = (id) => Product.findById(id);
+exports.getById = (_id) => Product.findById(id);
 exports.create = (data) => Product.create(data);
-exports.update = (id, data) =>
-  Product.findByIdAndUpdate(id, data, { new: true });
-exports.delete = (id) => Product.findByIdAndDelete(id);
+exports.update = (_id, data) =>
+  Product.findByIdAndUpdate(_id, data, { new: true });
+
+exports.delete = (_id) => Product.findByIdAndDelete(_id);
